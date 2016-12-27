a2dc88170a0261d1b1cceaf9a04ec44d912bff69
Gordon Evans <gjrevans@gmail.com>
2016-12-27 02:24:34 -0500

    Fixed bug with authenticity token.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,6 @@
 class ContactsController < ApplicationController
     skip_before_filter :verify_authenticity_token, :only => [:new, :create]
+    
     def new
         @projects = Project.all
         @contact = Contact.new

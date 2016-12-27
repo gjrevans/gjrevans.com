00da492ae217c1afa10fe7b4b43966a18b9a2525
Gordon Evans <gjrevans@gmail.com>
2016-12-27 02:27:35 -0500

    Fix issue with authenticity token.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,5 @@
 class ProjectsController < ApplicationController
+    skip_before_filter :verify_authenticity_token, :only => [:new, :create]
     before_action :set_project, only: [:show, :edit, :update, :destroy]
 
     def index

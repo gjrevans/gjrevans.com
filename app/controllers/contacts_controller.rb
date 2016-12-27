b3d00473c9b1b99da0788a0635318ed2397bcf2c
Gordon Evans <gjrevans@gmail.com>
2016-12-26 23:39:26 -0500

    Some styling changes.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,7 +5,7 @@ class ContactsController < ApplicationController
 
     def create
         ContactMailer.contact_confirmation(Contact.new(params[:contact])).deliver
-        flash[:success] = "Message Sent!"
+        flash[:success] = "Thanks, your message was sent!"
         redirect_to root_path
     end
 end

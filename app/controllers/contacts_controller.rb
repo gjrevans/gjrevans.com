615ab7670a4fefa391819af8efdd3467c9a6bb65
Gordon Evans <gjrevans@gmail.com>
2016-12-26 23:46:11 -0500

    Alert message styling.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,7 +5,7 @@ class ContactsController < ApplicationController
 
     def create
         ContactMailer.contact_confirmation(Contact.new(params[:contact])).deliver
-        flash[:success] = "Thanks, your message was sent!"
+        flash[:info] = "Thanks, your message was sent!"
         redirect_to root_path
     end
 end

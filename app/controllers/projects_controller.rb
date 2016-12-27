72fc39d7abe5f56330b0a6f5c65088dd156fd6d3
Gordon Evans <gjrevans@gmail.com>
2016-12-27 02:27:52 -0500

    Okay, fixed, again.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,5 @@
 class ProjectsController < ApplicationController
-    skip_before_filter :verify_authenticity_token, :only => [:new, :create]
+    skip_before_filter :verify_authenticity_token
     before_action :set_project, only: [:show, :edit, :update, :destroy]
 
     def index

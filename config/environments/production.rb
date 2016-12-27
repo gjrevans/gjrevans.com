e526148d3be844cb4a34c8c843b3482c1747ebb2
Gordon Evans <gjrevans@gmail.com>
2016-12-26 20:50:31 -0500

    Added production mailer stuffs.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,6 +56,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "homepage_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: ENV['DEFAULT_URL_HOST'] } if ENV['DEFAULT_URL_HOST'].present?
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.

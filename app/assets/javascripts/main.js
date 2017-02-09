5dff5c2656a965485bcb3827abda72e3e726b82e
Gordon Evans <gjrevans@gmail.com>
2017-02-08 22:10:11 -0500

    Moved GA code to main JS file.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,3 +1,13 @@
+/* -- GOOGLE ANALYTICS -- */
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-91672044-1', 'auto');
+ga('send', 'pageview');
+
+/* -- PAGE JS --*/
 $(document).ready(function(){
     // Minipulate navigation style based on position
     $(window).scroll(function(){
